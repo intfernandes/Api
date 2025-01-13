b92d8ce15820c7fe5a04713512f843141bf76df2
Lucas Fernandes <int2.fernandes@gmail.com>
2025-01-13 19:47:29 -0300

    set development db enviroment

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
+
+if(builder.Environment.IsDevelopment() ){
+// set db enviroment to development (sqlite on ./constance.db)
 builder.Services.AddDbContext<DataContext >(opt => {
 opt.UseSqlite( builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+} else {
+// set db enviroment to production
+builder.Services.AddDbContext<DataContext >(opt => {
+opt.UseSqlite( builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+}
  
 
 var app = builder.Build(); 
